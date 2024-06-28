9e70cd406747bd0384b4ac7a0e4a6c31123bc805
Selyss <99344963+Selyss@users.noreply.github.com>
2024-06-28 16:20:52 +0000

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Default, Debug)]
+#[clap(author=env!("CARGO_PKG_AUTHORS"), version)]
+struct Args {
+    file: String,
+
+}
+
+fn parse_markdown_file() {
+    todo!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("{:?}", args);
 }
